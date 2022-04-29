d771710b33e80d1dff6cdb957be8f6b7138e377b
Julie Marie <juliembe@stud.ntnu.no>
2022-04-29 17:06:08 +0200

    fix bug in pattern fit maximising function

--- asi_image_processing/pattern_fitting.py
+++ asi_image_processing/pattern_fitting.py
@@ -1,6 +1,9 @@
 from skimage import morphology
 from skimage.feature import canny, corner_harris, corner_peaks
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from time import time
 
 
 def plot_compute_corner_process(corners, square, coords):
@@ -39,3 +42,85 @@ def compute_corners(approximate_pattern, k=0.05, sigma=4, plot=False):
     if plot:
         plot_compute_corner_process(corners, square, coords)
     return corner_coords
+
+def sort_corners(corner_coords):
+    # Sorts the corner_coords in the order top_left, top_right, bottom_right, bottom_left
+    sorted_corners = corner_coords.copy()
+    sumall = np.sum(corner_coords, axis=1)
+    # The bottom right corner will always have the highest sum of x and y coords
+    # The top left corner will always have the lowest sum of x and y coords
+    for i, xy in enumerate(corner_coords):
+        if np.sum(xy) >= sumall.max():
+            sorted_corners[2] = xy
+        elif np.sum(xy) <= sumall.min():
+            sorted_corners[0] = xy
+        elif xy[0] > xy[1]:
+            sorted_corners[1] = xy
+        else:
+            sorted_corners[3] = xy
+    return sorted_corners
+
+def maximise_pattern_fit(bf_image, pattern, corner_coords, g=40):
+    '''
+    Maximising fit of mask by minimising standard deviation of the resulting
+    (masked) image.
+
+    bf_image =  hyperspy signal2D, recreated bright field image to mask with the pattern and
+                minimise std in
+    pattern =   pattern to use as a mask
+
+    corner_coords = corned coordinates
+
+    g = goal std reduction, default is 40 which is quite high.
+        Should be in the interval (10-50)
+    '''
+    img = bf_image.data
+    # Define the corners of the pattern
+    pts_pattern = np.array([[0, 0],
+                            [pattern.shape[1] - 1, 0],
+                            [pattern.shape[1] - 1, pattern.shape[0] - 1],
+                            [0, pattern.shape[0] - 1]])
+    pts = sort_corners(corner_coords)
+    homographyMat, status = cv2.findHomography(pts_pattern, pts)
+    mask = np.invert(cv2.warpPerspective(pattern, homographyMat,
+                                (img.shape[1], img.shape[0])))
+    resulting_image = img*mask
+    # plt.imshow(resulting_image, cmap='binary')
+    init_std = np.std(resulting_image)
+    goal = init_std-g
+
+    print(f'Initial std: {round(init_std,3)}')
+    print(f'Goal: {goal}')
+    w = 30
+    h = 30
+    milestone = goal+20
+    temp_pts = pts.copy()
+    new_pts = temp_pts.copy()
+    tic = time()
+    while np.std(resulting_image) > goal:
+        if np.std(resulting_image) < milestone:
+            print(f'Still working, but found {round(np.std(resulting_image),3)}')
+            milestone = np.std(resulting_image)
+            temp_pts = new_pts.copy()
+            if w > 2:
+                w -= 2
+            if h > 2:
+                h -= 2
+        new_pts = temp_pts.copy()
+        for i, point in enumerate(new_pts):
+            new_pts[i,0] = np.random.randint(temp_pts[i,0]-w, temp_pts[i,0]+w)
+            new_pts[i,1] = np.random.randint(temp_pts[i,1]-h, temp_pts[i,1]+h)
+        homographyMat, status = cv2.findHomography(pts_pattern, new_pts)
+        mask = np.invert(cv2.warpPerspective(pattern, homographyMat,
+                                             (img.shape[1], img.shape[0])))
+        resulting_image = img*mask
+        if time() - tic > 300:
+            homographyMat, status = cv2.findHomography(pts_pattern, temp_pts)
+            mask = np.invert(cv2.warpPerspective(pattern, homographyMat,
+                                                 (img.shape[1], img.shape[0])))
+            print(f'Timed out with std = {round(milestone,3)}')
+            break
+    toc = time()
+    print(f'Finished after {round(toc-tic,3)}s,')
+    print(f'with std = {round(np.min([np.std(resulting_image),milestone]),3)}.')
+    return mask
--- asi_image_processing/tests/test_pattern_fitting.py
+++ asi_image_processing/tests/test_pattern_fitting.py
@@ -9,3 +9,13 @@ def test_compute_corners():
     image[   :,-10:] = 0
     coords = aippf.compute_corners(image)
     assert coords.shape[0] == 4
+
+def test_sort_corners():
+    image = np.ones((100,100))
+    image[:10 , :  ] = 0
+    image[:   , :10] = 0
+    image[-10:,   :] = 0
+    image[   :,-10:] = 0
+    coords = aippf.compute_corners(image)
+    sorted_coords = aippf.sort_corners(coords)
+    assert sorted_coords.shape[0] == 4
